3b2cc02b69f478a245fa2931995ded6b3dcbe75a
Mathijs Kemp <mathijskemp@gmail.com>
2025-04-07 18:30:00 +0200

    initial commit including project files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonInput,
+  IonModal,
+  IonPage,
+  IonText,
+  IonTextarea,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
+import { useState } from "react";
 
 const Home: React.FC = () => {
+  const [showModal, setShowModal] = useState(true);
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +30,30 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={() => setShowModal(true)}>OPEN MODAL</IonButton>
+
+        <IonModal
+          initialBreakpoint={0.6}
+          breakpoints={[0.6, 1]}
+          isOpen={showModal}
+          expandToScroll={false}
+          onDidDismiss={() => setShowModal(false)}
+        >
+          <IonContent>
+            <IonText>
+              <p>This is a modal!!</p>
+            </IonText>
+          </IonContent>
+          <IonFooter>
+            <IonToolbar>
+              <IonTextarea placeholder="This is a textarea" />
+              {/* <IonInput placeholder="This is an INPUT" /> */}
+              <IonButtons>
+                <IonButton onClick={() => setShowModal(false)} />
+              </IonButtons>
+            </IonToolbar>
+          </IonFooter>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
